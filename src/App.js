d5259665c495b6eebeb2ee81084066a9719e23e8
Ashraf <ashraf.dham08@gmail.com>
2024-05-19 12:07:44 +0300

    quote generating app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState} from 'react';
 
 function App() {
+ const [quote,setQuote] = useState({})
+
+  function fetchQuote(){
+    fetch("https://api.quotable.io/random")
+    .then(response => response.json())
+    .then(data => {console.log(data);setQuote(data)})
+
+  }
+
+ 
+  useEffect(() => {
+    fetchQuote()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className='container'>
+    <h1 className='quote'>"{quote.content}"</h1>
+    <h2 className='author'>{quote.author}</h2>
+    <button onClick={fetchQuote} className='btn'> Generate quote</button>
+  </div>
   );
 }
 
